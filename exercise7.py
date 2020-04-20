9049cd604113b88fa8f420af3356eae2f1b5b82c
piotrektnw <grafomat@o2.pl>
2020-04-20 10:43:47 +0200

    Create exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,9 @@
+ #Return the total count of string “Emma” appears in the given string
+ 
+ astring = "Emma is good developer. Emma is a writer"
+
+howmany = astring.count("Emma")
+
+output = ("Emma appeared {} times.")
+
+print(output.format(howmany))
