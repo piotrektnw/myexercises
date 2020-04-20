d54b599a6142a4149ac9c47e9876af18305d8ac1
piotrektnw <grafomat@o2.pl>
2020-04-20 10:46:32 +0200

    Update exercise7.py

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,15 @@
 sampleStr = "Emma is good developer. Emma is a writer"
 cnt = sampleStr.count("Emma")
 print(cnt)
+
+# or without using any string function 
+
+def count_jhon(statement):
+    print("Given String: ", statement)
+    count = 0
+    for i in range(len(statement) - 1):
+        count += statement[i: i + 4] == 'Emma'
+    return count
+
+count = count_jhon("Emma is good developer. Emma is a writer")
+print("Emma appeared ", count, "times")
