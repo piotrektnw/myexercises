4be2c7069d8080c6660dda7810bf0433164c83d5
piotrektnw <grafomat@o2.pl>
2020-04-23 17:20:10 +0200

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,15 +1,11 @@
 #Given a range of first 10 numbers, Iterate from start number to the end number 
 #and print the sum of the current number and previous number
 
-x = range(10)
-curr = "Current number: "
-prev = " Previous number: "
-sum = " Sum: "
+def myfunc():
+  for x in range(1, 10, 1):
+    current_num = x
+    previous_num = 0 + x - 1
+    summary = current_num + previous_num
+    print("Current number:", current_num, "Previous num: ", previous_num, "Sum: ", summary)
 
-print("Printing current and previous number sum in a given range(10)")
-print("Current Number: 0 Previous Number: 0 Sum: 0")
-for x in range(1, 10, 1):
-    print(curr + str(x) + prev + str(x - 1) + sum + str(x + (x - 1)))
-
-
-
+myfunc()
