7cf14070ca8cda15eceb7417ff8cdedc790b8a38
piotrektnw <grafomat@o2.pl>
2020-04-24 09:09:42 +0200

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,17 +1,14 @@
 #Given a string and an integer number n, remove characters from a 
 #string starting from zero up to n and return a new string
 
+def removeChars(txt, n):
 
-counter = 0
-while counter < 10:
-    enter_string = input("Enter a string: ")
-    n = input("Enter a number: ")
-    if int(n) > len(enter_string):
-        print("Too long. It must be less than: ", len(enter_string))
-        counter += 1
-    else:
-        print(enter_string[4:])
+  if n > len(txt):
+    print("Too long")
+  else:
+    print(txt[n:])
 
+removeChars("pynative", 4) 
 
 
 
