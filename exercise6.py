b5d300a5bd10d4cde2e8ba280fd26ee085b7705e
piotrektnw <grafomat@o2.pl>
2020-04-24 09:31:06 +0200

    Update exercise6.py

--- exercise6.py
+++ exercise6.py
@@ -1,9 +1,12 @@
 Given a list of numbers, Iterate it and print only those numbers which are divisible of 5
 
-list1 = [10, 20, 33, 46, 55]
+list_a = [10, 20, 33, 46, 55]
 
-print("Given list is ", list1)
+def division_by_five(list_1):
+    for x in list_1:
+        if x % 5 == 0:
+            print(x)
+
+print("Given list is", list_a)
 print("Divisible of 5 in a list ")
-for x in list1:
-    if x % 5 == 0:
-        print(x)
+division_by_five(list_a)
