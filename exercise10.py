671cac65c39bbed66ee4bee9d65d401816317b40
piotrektnw <grafomat@o2.pl>
2020-04-29 16:49:33 +0200

    Update exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -1,16 +1,22 @@
-first_list = [10, 20, 23, 11, 17]
-second_list = [13, 43, 24, 36, 12]
-new_list = []
-for x in first_list:
-    if (x % 2) != 0:
-        new_list.append(x) #inspired by pynative solution
-for y in second_list:
-    if (y % 2) == 0:
-        new_list.append(y)
-
-print(new_list)
 
 
+def odd_even(first_l, second_l):
+    print("First list is", first_l)
+    print("Second list is", second_l)
+    new_l = []
+    for x in first_l:
+        if (x % 2) != 0:
+         new_l.append(x)
+    for y in second_l:
+        if (y % 2) == 0:
+         new_l.append(y)
+    print("Mixed lists are: ", new_l)
+
+
+first_l = [10, 20, 23, 11, 17]
+second_l = [13, 43, 24, 36, 12]
+new_l = []
+odd_even(first_l, second_l)
 
 
 
