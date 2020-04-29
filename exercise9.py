6678cc8fb4299c10da943f8ceb153d6420321bed
piotrektnw <grafomat@o2.pl>
2020-04-29 16:31:12 +0200

    Update exercise9.py

--- exercise9.py
+++ exercise9.py
@@ -1,28 +1,14 @@
 #Reverse a given number and return true if it is the same as the original number
 
-var1 = "121"
-rev_var1 = var1[::-1]
-var2 = "123"
-rev_var2 = var2[::-2]
-check_item = True
-
-if var1 == rev_var1:
-    check_item = True
-else:
-    check_item = False
-
-print("Original number: ", var1)
-if check_item == True:
-    print("The original and reverse number is the same:", check_item)
-else:
-    print("The original and reverse number is the same:", check_item)
-print("Original number: ", var2)
-if var2 == rev_var2:
-    check_item = True
-else:
-    check_item = False
-
-if check_item == True:
-    print("The original and reverse number is the same:", check_item)
-else:
-    print("The original and reverse number is the same:", check_item)
+def reversecheck(num):
+    original_num = num
+    print("Original number is: ", original_num)
+    rev_num = num[::-1]
+    if num == rev_num:
+        score = True
+    else:
+        score = False
+    print("The original and reverse num is the same:", score)
+    
+reversecheck("121")
+reversecheck("221")
